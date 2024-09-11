b900e645761ec5fc2be3204dcc065913b4c2ea7a
AversionToDeepWater <imanabdelgani@gmail.com>
2024-09-11 20:36:33 +0100

    Drop in session function exercise start

---
+++ DropInSessionsPython/fuctionexercise.py
@@ -0,0 +1,13 @@
+
+# Make a function that takes the argument 'aNumber' and loops from 0 till 42
+# Once it hits 42 it should say 'The meaning of life!' and the stop looping
+
+# i = 0
+# #i = i+1 ???
+#  def recap(aNumber):
+#     if int(aNumber) > 42:
+#         aNumber = aNumber + 1
+#
+#     elif int(aNumber) < 42:
+#         a
+
