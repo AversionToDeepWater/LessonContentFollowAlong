e2fe40768b3a9c49f900f7e0613fa58ae27376d5
AversionToDeepWater <imanabdelgani@gmail.com>
2024-09-16 00:07:23 +0100

    API assignment attempt -- forgot to do in assignment folder

--- PythonLesson6API/mainAPI.py
+++ PythonLesson6API/mainAPI.py
@@ -1,4 +1,5 @@
 #Calling your API
+from pprint import pprint
 
 #First you need to import requests library, otherwise you can't call your API.
 #All imports and requests should be at the top of your code
@@ -13,7 +14,33 @@ response = requests.get(endpoint).json()
 #For the actual data, which will come from the internet as a JSON file
 #For Python to understand this, you need to use inbuilt JSON function to import JSON package in Python script
 #data = response.json()
-print(response)
+#print(response)
 
-# for artworks in response:
-#      print(response['title'])
+total_artworks = response['pagination']['total'] #total no of artworks
+
+#Figuring out what data from JSON data I want
+title = response['data'][0]['title'] #title of the first artwork - data is a list, [0] for first item in list, 'title' is key
+description = response['data'][0]['description'] #description of first artwork, too long
+short_description = response['data'][0]['short_description'] #short description of first artwork which I prefer to use
+artist_display = response['data'][0]['artist_display'] #Artist name, and info
+# print(artist_display)
+
+def get_description(response:str, i:int): #get short description of artwork based on lucky no?
+    short_description = response['data'][i]['short_description']
+    return short_description
+
+def get_artist_info(response:str): #returns list of titles and artist info from API
+    for item in response['data']:
+        print(item['title'], item['artist_display'])
+    return
+
+get_artist_info(response)
+
+
+
+
+print(get_description(response,0))
+
+# art = response[0]['title']
+#
+# print(art)
