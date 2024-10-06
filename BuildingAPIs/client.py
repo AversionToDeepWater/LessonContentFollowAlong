9b179f96ac093e5dc60792ceee72e17c0fed0425
AversionToDeepWater <imanabdelgani@gmail.com>
2024-10-07 00:25:56 +0100

    Adding useful notes to client.py

--- BuildingAPIs/client.py
+++ BuildingAPIs/client.py
@@ -10,13 +10,16 @@ import requests
 
 #to split screen, right click an active open .py file and press 'split right'
 
-def get_characters():
+#for assignment, will need a def run() function
+def get_characters(): #ADVANCED - can have the root (e.g. /characters) as an input to the function
     endpoint = "http://127.0.0.1:8000/characters"
     data = requests.get(endpoint).json()
     return("Client says: ", data)
 
 #ONLY RUN FUNCTIONS AFTER IF NAME == MAIN!! Otherwise, very bad for imports
 if __name__ == "__main__":
-    print(get_characters())
+    print(get_characters()) #run function will need to be here for assignment!!
+
+
 
 
