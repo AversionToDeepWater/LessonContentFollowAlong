acbb8086efa86fa262b4141c915771ddd8a68d71
AversionToDeepWater <imanabdelgani@gmail.com>
2024-11-17 14:29:19 +0000

    trying to import assets

--- PyGameTutorials/PyGameNotes.md
+++ PyGameTutorials/PyGameNotes.md
@@ -1,8 +1,21 @@
 # Making a game in PyGame!!
 
 ## Surfaces 
-Need to create a **display surface**, this is our game window
+Need to create a **display surface**, this is our game window 
+
+* Must be unique, is always visible 
 
 Then we have a **regular surface** which is essentially a single image
 
 * This can be something imported, rendered text or a plain colour
+* Can have as many regular surfaces as you want
+* Only shown when connected to the display surface
+
+To show regular surface on display surface use:
+`screen.blit(surface, position)`
+
+where blit stands for Block Image Transfer 
+
+takes 2 arguments, surface we want to show and the position we want it 
+
+In pygame, coordinates for screen start in top left i.e., origin 0,0 is top left
--- PyGameTutorials/PyGameTutorialYT.py
+++ PyGameTutorials/PyGameTutorialYT.py
@@ -12,6 +12,12 @@ pygame.display.set_caption('DOGTECTIVE') #the title of our game!
 #controlling frame rate - saying the min and max frame rate
 clock = pygame.time.Clock()
 
+#MAKING SURFACES
+#Plain surface!
+#will need a tuple with width and height of surface, similar to screen
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')  #fill our surface, there is a list of colours you can use on pygame website
+
 
 #MAKING EVENT LOOP
 while True:
@@ -21,7 +27,7 @@ while True:
         if event.type == pygame.QUIT: #i.e closing window
             pygame.quit() #this is the opposite of pygame.init()
             exit() #this will stop the code running, more secure than break for while loop - import from sys
-
+    screen.blit(test_surface,(200,100))
     # update everything and display surface
     pygame.display.update() # only have to call it once, like pygame.init()
     clock.tick(60) #saying game should not run faster than 60fps - need to make sure game is not too complex
