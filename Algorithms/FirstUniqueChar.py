fb270977a3dae23270dab36cc711458ed28ac04e
AversionToDeepWater <imanabdelgani@gmail.com>
2024-11-26 19:23:06 +0000

    Algorithm lesson, continuing unique char challenge

--- Algorithms/FirstUniqueChar.py
+++ Algorithms/FirstUniqueChar.py
@@ -4,3 +4,10 @@
 # Given an input string, find the first non-repeating character
 
 s = 'aabccbdcbe'
+
+def FirstUniqueChar(string:str) -> str:
+    for s in string:
+        if string.count(s) == 1:
+            return s
+
+print(FirstUniqueChar(s))
