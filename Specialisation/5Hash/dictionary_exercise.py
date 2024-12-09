a8dc18a02c27c66e5079900a14a91d74165f9d54
AversionToDeepWater <imanabdelgani@gmail.com>
2024-12-09 00:52:34 +0000

    dictionary exercise hash tables

---
+++ Specialisation/5Hash/dictionary_exercise.py
@@ -0,0 +1,16 @@
+"""
+Python program to find the sum of all items in a dictionary.
+
+Input : {'a': 100, 'b':200, 'c':300}
+Output : 600
+"""
+
+example =  {'a': 100, 'b':200, 'c':300}
+
+def value_output (d:dict):
+    total = 0
+    for key in d: #iterates through dictionary
+        total += d[key] #adds the value from each key value pair to starting value of zero
+    return total
+
+print(value_output(example))
